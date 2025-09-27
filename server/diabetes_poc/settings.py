43e077f28d32669152381acd4cd6c38e84992e8a
IQSujit <sujitkumar@MacBookPro.local>
2025-09-27 14:28:46 +0530

    added static root

--- server/diabetes_poc/settings.py
+++ server/diabetes_poc/settings.py
@@ -93,6 +93,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
